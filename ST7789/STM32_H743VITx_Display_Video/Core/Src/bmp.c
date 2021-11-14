43d3680e19c2c285a7f9d679270906fd3045dfa6
UlmerPavel <ulmerpavel@mail.ru>
2021-11-14 15:44:17 +0300

    Исправил ориентацию картинки

--- ST7789/STM32_H743VITx_Display_Video/Core/Src/bmp.c
+++ ST7789/STM32_H743VITx_Display_Video/Core/Src/bmp.c
@@ -37,6 +37,7 @@ IMGRESULT DrawBMPImageFile(const char* fname, uint16_t x_pos, uint16_t y_pos)
       f_close(&img_file);
       return IMG_FIL_ERR;
     }
+  f_close(&img_file);
 
   BmpFileHaeder = bmp_file_bufer;
   BmpFileInfo   = bmp_file_bufer + 0x0E;
@@ -61,69 +62,55 @@ IMGRESULT DrawBMPImageFile(const char* fname, uint16_t x_pos, uint16_t y_pos)
     direction = 1;
   }
 
+  //Выделяем буфер для картинки
+  void *bmp_frame_bufer = malloc(imageWidth*imageHeight*sizeof(uint16_t));
+  if(bmp_frame_bufer == NULL)
+    return NOT_ENOUGH_SPACE;
+
   //передвинем указатель на начало битового поля
-//  res = f_lseek(&img_file, BmpFileHaeder->bfOffBits);//imageOffset);
-//  if(res != FR_OK){
-//    f_close(&img_file);
-//    return IMG_FIL_ERR;
-//  }
   uint8_t* pix_pointer = (uint8_t*) (bmp_file_bufer + BmpFileHaeder->bfOffBits);
 
-  //Массив байт для чтения из флеши
-  //static uint8_t imageRow[(240 * 3 + 3) & ~3];
-
-  //Массив байт для одной горизонтальной линии, для загрузки в дисплей
-  static uint16_t PixBuff[240];
-
-
-  uint32_t y_start;
-  uint32_t y_end;
+  int32_t y_start;
+  int32_t y_end;
   if(direction == -1){
     /* Рисуем строки снизу вверх */
-    y_start = imageHeight+y_pos-1;
-    y_end = y_pos-1;
+    y_start = imageHeight;
+    y_end = 0;
   }
   else{
     /* Рисуем строки сверху вниз вверх */
-    y_start = y_pos-1;
-    y_end = imageHeight+y_pos-1;
+    y_start = 0;
+    y_end = imageHeight;
   }
 
   /* Рисуем строки */
-  for (uint32_t y=y_start; y!=y_end; y+=direction)
+  for (int32_t y=y_start; y!=y_end; y+=direction)
   {
-//    res = f_read(&img_file, imageRow, (imageWidth * 3 + 3) & ~3, &bytesRead);
-//    if (res != FR_OK)
-//    {
-//      f_close(&img_file);
-//      return IMG_FIL_ERR;
-//    }
-
-    //Подготавливаем буфер строки картинки для вывода
     //перепаковка из формата BMP24 бита в формат RGB565
-    //imageRow и PixBuff ссылаются на один буфер, но так как результат перепаковки уменьшается,
-    //то так поступить можно, уменьшая затраченную памть
     uint8_t* ppointer = (uint8_t*)pix_pointer;
+    uint16_t* PixBuff = (uint16_t*)bmp_frame_bufer + y*imageWidth;
     for (uint32_t x = 0; x < imageWidth; x++)
     {
       uint8_t b = *ppointer++;
       uint8_t g = *ppointer++;
       uint8_t r = *ppointer++;
-      PixBuff[x] = RGB565(r, g, b);
+      *PixBuff = RGB565(r, g, b);
 
       //свапаем пиксель
-      uint16_t temp = PixBuff[x];
-      PixBuff[x] = PixBuff[x] >> 8;
-      PixBuff[x] |= (temp << 8);
+      uint16_t temp = *PixBuff;
+      *PixBuff = *PixBuff >> 8;
+      *PixBuff |= (temp << 8);
+      PixBuff++;
     }
-    // Рисуем полученную строку
-    ST7789_DrawImage( x_pos, y, imageWidth, 1, PixBuff );
-
 
     pix_pointer += (imageWidth * 3 + 3) & ~3;
   }
 
   free(bmp_file_bufer);
-  f_close(&img_file);
+
+  // Рисуем полученную картинку
+  ST7789_DrawImage( x_pos, y_pos, imageWidth, imageHeight, (uint16_t*)bmp_frame_bufer );
+
+  free(bmp_frame_bufer);
   return IMG_OK;
 }
